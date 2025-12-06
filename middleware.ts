8bd52995be99b5d7a4a447d0cbd7a9e2caad5414
Evan Huang <huangshaoqi8888@gmail.com>
2025-12-07 07:53:35 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,27 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+import { jwtVerify } from "jose"
 
-export function middleware(request: NextRequest) {
-  const hasValidPassword = request.cookies.has('access_token')
-  
-  // Allow access to the password entry page
-  if (request.nextUrl.pathname === '/password') {
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+
+  if (pathname === "/password") return NextResponse.next()
+
+  const token = req.cookies.get("access_token")?.value
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/password", req.url))
+  }
+
+  try {
+    const secret = new TextEncoder().encode(process.env.ACCESS_JWT_SECRET)
+    await jwtVerify(token, secret)
     return NextResponse.next()
+  } catch (err) {
+    return NextResponse.redirect(new URL("/password", req.url))
   }
-
-  // Redirect to password page if no valid password cookie
-  if (!hasValidPassword) {
-    return NextResponse.redirect(new URL('/password', request.url))
-  }
-
-  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!_next|static|favicon.ico).*)"],
 }
