c47a68b0b2599b45445111d25e1fa885bd86a33f
Evan Huang <huangshaoqi8888@gmail.com>
2025-12-07 07:52:51 +0800

    Update route.ts

--- app/api/verify-password/route.ts
+++ app/api/verify-password/route.ts
@@ -1,20 +1,30 @@
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server"
+import { SignJWT } from "jose"
 
-export async function POST(request: Request) {
-  const { password } = await request.json()
+export async function POST(req: Request) {
+  const { password } = await req.json()
 
-  if (password === process.env.ACCESS_PASSWORD) {
-    const response = NextResponse.json('OK', { status: 200 })
-    response.cookies.set('access_token', 'true', {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
-      maxAge: 5 * 60,
-    })
-
-    return response
+  if (password !== process.env.ACCESS_PASSWORD) {
+    return new NextResponse("Forbidden", { status: 403 })
   }
 
-  return new NextResponse('Unauthorized', { status: 401 })
+  const secret = new TextEncoder().encode(process.env.ACCESS_JWT_SECRET)
+
+  const token = await new SignJWT({ role: "access" })
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime("5m")
+    .setIssuedAt()
+    .sign(secret)
+
+  const res = NextResponse.json({ success: true })
+
+  res.cookies.set("access_token", token, {
+    httpOnly: true,
+    path: "/",
+    maxAge: 60 * 5,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+  })
+
+  return res
 }
