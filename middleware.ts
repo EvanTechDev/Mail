447bc140a4ecd666cab2827e9320bbb36028e734
Evan Huang <huangshaoqi8888@gmail.com>
2025-12-06 12:47:09 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,22 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { SESSIONS } from './lib/sessions'
+import jwt from 'jsonwebtoken'
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('access_token')?.value
 
   if (!token) {
-    return NextResponse.redirect('/password')
+    return NextResponse.redirect(new URL('/password', req.url))
   }
 
-  const expires = SESSIONS.get(token)
-
-  if (!expires || expires < Date.now()) {
-    return NextResponse.redirect('/password')
+  try {
+    jwt.verify(token, process.env.JWT_SECRET!)
+    return NextResponse.next()
+  } catch (e) {
+    return NextResponse.redirect(new URL('/password', req.url))
   }
-
-  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/', '/protected/:path*'],
 }
